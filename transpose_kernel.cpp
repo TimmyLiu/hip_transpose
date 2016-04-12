56b3a18c0765532fd32af2bcceeb344632a74225
Timmy Liu <Timmy.Liu@amd.com>
2016-04-12 08:08:17 -0500

    add prototype for template specialization

--- transpose_kernel.cpp
+++ transpose_kernel.cpp
@@ -1,1 +1,9 @@
 #include "transpose_kernel.h"
+
+// defination of template specialization
+template<>
+__global__ void transpose_kernel<float, 1, 1>(hipLaunchParm lp, float *input_matrix, float *output_matrix, size_t input_row_size, size_t input_col_size,
+                                        size_t batch_size)
+{
+
+}
--- transpose_kernel.h
+++ transpose_kernel.h
@@ -64,4 +64,9 @@ inline __global__ void transpose_kernel(hipLaunchParm lp, T *input_matrix, T *ou
 
 }
 
+//the declaration of specialization
+template<>
+__global__ void transpose_kernel<float, 1, 1>(hipLaunchParm lp, float *input_matrix, float *output_matrix, size_t input_row_size, size_t input_col_size,
+                                        size_t batch_size);
+
 #endif
